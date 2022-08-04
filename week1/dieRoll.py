5deca2741ae2d403b721abdb7f50af3c829bc7a1
An Nguyen <roryn4545@gmail.com>
2022-08-04 13:06:05 -0500

    updates

--- week1/dieRoll.py
+++ week1/dieRoll.py
@@ -1,8 +1,10 @@
 import random
+try:
+    print("Let's roll a dice!")
+    die = input('How many sides should it have? (2-20): ')
+    die = int(die)
 
-print("Let's roll a dice!")
-die = input('How many sides should it have? (2-20): ')
-die = int(die)
-
-print("It's rolling... ")
-print("It's a " + str(random.randint(1,die)))
+    print("It's rolling...")
+    print("It's a " + str(random.randint(0,die)))
+except:
+    print('Enter a valid number!')
--- week1/evenOdd.py
+++ week1/evenOdd.py
@@ -1,7 +1,10 @@
 #Even Odd
-number = int(input('Enter a number :'))
+try:
+    number = int(input('Enter a number :'))
 
-if number % 2 == 0:
-    print("It's Even!")
-elif number % 2 == 1:
-    print("It's Odd!")
+    if number % 2 == 0:
+            print("It's Even!")
+    elif number % 2 == 1:
+            print("It's Odd!")
+except:
+    print('Enter a valid number!')
