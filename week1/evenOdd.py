46880088194a424d1153209e35691bf8b82a695c
An Nguyen <roryn4545@gmail.com>
2022-08-03 19:44:31 -0500

    evenOdd HW

--- week1/evenOdd.py
+++ week1/evenOdd.py
@@ -1,7 +1,23 @@
-#Even Odd
-number = int(input('Enter a number :'))
+import random
+#Allows for use of random module
 
-if number % 2 == 0:
-    print("It's Even!")
-elif number % 2 == 1:
-    print("It's Odd!")
+print("Let's roll a dice!")
+#Initial message
+diceIn = input('How many sides should it have? (2-20): ')
+#assigns user input into diceIn string
+
+try:
+    dice = int(diceIn)
+except:
+    print("That's not a number!")
+    exit()
+#Converts string into int and discards invalid entries.
+
+if dice > 1 and dice <= 20:
+    print("It's rolling...")
+    result = random.randint(1, dice)
+    print("It's a " + str(result) + "!")
+#Checks parameters, fetches result, and displays as string.   
+else:
+    print("That's not a valid number!")
+#Message if parameters are not met.
